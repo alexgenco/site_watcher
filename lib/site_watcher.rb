9f35aaabf9ea96fce46ddbe25f1a55f5e63db61c
Alex Genco <alexgenco@gmail.com>
2014-11-30 13:36:42 -0800

    remove tests, use new dsl structure

--- lib/site_watcher.rb
+++ lib/site_watcher.rb
@@ -3,76 +3,121 @@ require "open-uri"
 require "nokogiri"
 
 class SiteWatcher
-  class CSSTest
-    def initialize(html)
-      @html = html
-      @results = []
-    end
+  def self.watch(opts={}, &block)
+    sleep_interval = opts.fetch(:sleep, 0)
 
-    def pass?
-      @results.all?
-    end
+    new([]).tap do |instance|
+      DSL::Top.new(instance).instance_eval(&block)
+    end.watch(sleep_interval)
+  end
 
-    def includes(css)
-      @results << !!@html.at_css(css)
-      self
-    end
+  attr_accessor :pages
 
-    def excludes(css)
-      @results << !@html.at_css(css)
-      self
+  def initialize(pages)
+    @pages = pages
+  end
+
+  def watch(sleep_interval)
+    @pages.cycle do |page|
+      if page.reset.fulfilled?
+        @pages.delete(page)
+      else
+        sleep(sleep_interval)
+      end
     end
   end
 
-  class Page
-    def initialize(content)
-      @html = Nokogiri::HTML(content)
-      @tests = []
-    end
+  module DSL
+    class Top
+      def initialize(watcher)
+        @watcher = watcher
+      end
 
-    def css
-      CSSTest.new(@html).tap do |css|
-        @tests << css
+      def page(url, &block)
+        Page.new(url).tap do |page|
+          page.instance_eval(&block)
+          @watcher.pages << page
+        end
       end
     end
 
-    def tests_pass?
-      @tests.all?(&:pass?)
+    class Page
+      attr_reader :url
+
+      def initialize(url)
+        @url = url
+        @tests = []
+      end
+
+      def reset
+        @document = nil
+        self
+      end
+
+      def document
+        @document ||= Nokogiri::HTML(open(@url))
+      end
+
+      def fulfilled?
+        tests = @tests.map { |prc| Test.new(self, &prc) }
+
+        if tests.all?(&:fulfilled?)
+          @fulfilled.call if @fulfilled.respond_to?(:call)
+          true
+        else
+          false
+        end
+      end
+
+      def test(&block)
+        @tests << block
+      end
+
+      def fulfilled(&block)
+        @fulfilled = block
+      end
     end
-  end
 
-  class Result
-    def success(&block)
-      return @success unless block_given?
-      @success = block
+    class Test
+      def initialize(page, &block)
+        @page = page
+        @css = []
+        instance_eval(&block)
+      end
+
+      def fulfilled?
+        @css.all?(&:fulfilled?)
+      end
+
+      def css
+        CSS.new(@page.document).tap do |css|
+          @css << css
+        end
+      end
     end
 
-    def failure(&block)
-      return @failure unless block_given?
-      @failure = block
-    end
-  end
+    class CSS
+      def initialize(document)
+        @document = document
+        @exclusions = []
+        @inclusions = []
+      end
 
-  def initialize(url, &test_handler)
-    raise(ArgumentError, "block required") unless block_given?
+      def fulfilled?
+        @inclusions.all? do |css|
+          @document.at_css(css)
+        end && @exclusions.all? do |css|
+          !@document.at_css(css)
+        end
+      end
 
-    @url = url
-    @test_handler = test_handler
-  end
+      def excludes(str)
+        @exclusions << str
+      end
 
-  def watch(&result_handler)
-    raise(ArgumentError, "block required") unless block_given?
-
-    page = Page.new(open(@url))
-    @test_handler.call(page)
-
-    result = Result.new
-    result_handler.call(result)
-
-    if page.tests_pass?
-      result.success.call(@url)
-    else
-      result.failure.call(@url)
+      def includes(str)
+        @inclusions << str
+      end
     end
   end
 end
