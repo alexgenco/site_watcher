4cc9101c39550b3057ef67546f9e21265a1ee3fa
Alex Genco <alexgenco@gmail.com>
2014-12-06 22:46:45 -0800

    use rspec and capybara for expectations DSL

--- lib/site_watcher.rb
+++ lib/site_watcher.rb
@@ -1,27 +1,36 @@
 require "site_watcher/version"
+
+require "capybara"
+require "rspec/expectations"
 require "open-uri"
-require "nokogiri"
 require "json"
 
 class SiteWatcher
   def self.watch(opts={}, &block)
     trap(:SIGINT) { abort(?\n) }
 
-    sleep_interval = opts.fetch(:sleep, 5)
-
+    delay = opts.fetch(:every, 5)
     dsl = DSL::Top.new
     dsl.instance_eval(&block)
-    new(dsl.pages).watch(sleep_interval)
+
+    new(dsl.pages).watch(delay)
   end
 
   def initialize(pages)
     @pages = pages
   end
 
-  def watch(sleep_interval)
+  def watch(delay)
     @pages.cycle do |page|
-      @pages.delete(page) if page.fulfilled?
-      sleep(sleep_interval)
+      #begin
+        page.__run!
+        @pages.delete(page)
+      #rescue ::RSpec::Expectations::ExpectationNotMetError
+      #rescue => e
+        #$stderr.puts("Exception on #{page.url}: #{e.inspect}")
+      #end
+
+      sleep(delay) if @pages.last == page
     end
   end
 
@@ -42,25 +51,12 @@ class SiteWatcher
     end
 
     class Page
+      include ::RSpec::Matchers
       attr_reader :url
 
       def initialize(url)
         @url = url
         @tests = []
-      end
-
-      def fulfilled?
-        tests = @tests.map { |prc| Test.new(self, &prc) }
-
-        if tests.all?(&:fulfilled?)
-          @fulfilled.call if @fulfilled.respond_to?(:call)
-          true
-        else
-          false
-        end
-      rescue EOFError, OpenURI::HTTPError => e
-        $stderr.puts "Error on #{@url}: #{e.inspect}"
-        false
       end
 
       def test(&block)
@@ -70,125 +66,19 @@ class SiteWatcher
       def fulfilled(&block)
         @fulfilled = block
       end
-    end
 
-    class Test
-      def initialize(page, &block)
-        @page = page
-        @css = []
-        @json = []
-        instance_eval(&block)
-      end
-
-      def fulfilled?
-        @css.all?(&:fulfilled?) &&
-          @json.all?(&:fulfilled?)
-      end
-
-      def css
-        CSS.new(@page.url).tap do |css|
-          @css << css
-        end
-      end
-
-      def json
-        JSON.new(@page.url).tap do |json|
-          @json << json
-        end
-      end
-    end
-
-    class CSS
-      def initialize(url)
-        @url = url
-        @exclusions = []
-        @inclusions = []
-      end
-
-      def fulfilled?
-        @document = nil
-        @inclusions.all? do |css|
-          document.at_css(css)
-        end && @exclusions.all? do |css|
-          !document.at_css(css)
-        end
-      end
-
-      def document
-        @document ||= Nokogiri::HTML(open(@url))
-      end
-
-      def excludes(str)
-        @exclusions << str
-        self
-      end
-
-      def includes(str)
-        @inclusions << str
-        self
-      end
-    end
-
-    class JSON
-      class Test
-        def initialize(url, path)
-          @path = path
-          @url = url
-          @includes = []
-          @excludes = []
-        end
-
-        def fulfilled?
-          @content = nil
-
-          @includes.all? do |incl|
-            case incl
-            when String
-              content.include?(incl)
-            when Regexp
-              content =~ incl
-            end
-          end && @excludes.all? do |excl|
-            case excl
-            when String
-              !content.include?(excl)
-            when Regexp
-              content !~ excl
-            end
+      def __run!
+        open(@url) do |response|
+          case response.content_type
+          when /json/i
+            page = ::JSON.parse(response.read, :symbolize_names => true)
+          else
+            page = ::Capybara::Node::Simple.new(response.read)
           end
         end
 
-        def content
-          @content ||= begin
-            json = ::JSON.load(open(@url))
-            @path.inject(json, :fetch)
-          end
-        end
-
-        def excludes(str_or_regex)
-          @excludes << str_or_regex
-          self
-        end
-
-        def includes(str_or_regex)
-          @includes << str_or_regex
-          self
-        end
-      end
-
-      def initialize(url)
-        @url = url
-        @tests = []
-      end
-
-      def fulfilled?
-        @tests.all?(&:fulfilled?)
-      end
-
-      def at(*path)
-        Test.new(@url, path).tap do |test|
-          @tests << test
-        end
+        @tests.each { |test| test.call(page) }
+        @fulfilled.call(@url) if @fulfilled.respond_to?(:call)
       end
     end
   end
--- site_watcher.gemspec
+++ site_watcher.gemspec
@@ -15,7 +15,8 @@ Gem::Specification.new do |spec|
   spec.files         = `git ls-files -z`.split("\x0")
   spec.require_paths = ["lib"]
 
-  spec.add_dependency "nokogiri"
+  spec.add_dependency "capybara"
+  spec.add_dependency "rspec-expectations"
   spec.add_development_dependency "bundler", "~> 1.7"
   spec.add_development_dependency "rake", "~> 10.0"
 end
